15c94e5d36d95bfd928d2e3407cd81161d9bf298
shhu21 <hu.sara21@gmail.com>
2020-10-09 00:54:57 -0700

    adds friend and thought form

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
-import { QUERY_THOUGHTS } from '../utils/queries';
 import ThoughtList from '../components/ThoughtList.js/index.js';
 import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 import Auth from '../utils/auth';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
 const Home = () => {
   // use useQuery hook to make query request
@@ -19,13 +19,18 @@ const Home = () => {
 
   return (
     <main>
-      <div className="flex-row justify-space-between">
+      <div className='flex-row justify-space-between'>
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            <ThoughtForm />
+          </div>
+        )}
         <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
           ) : (
-            <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
-          )}
+              <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+            )}
         </div>
         {loggedIn && userData ? (
           <div className="col-12 col-lg-3 mb-3">
